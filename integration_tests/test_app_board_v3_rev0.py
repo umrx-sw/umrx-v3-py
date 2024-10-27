ad29989d68ec5e704776820ddee20ae4a42e6c14
Konstantin Selyunin <selyunin.k.v@gmail.com>
2024-10-27 21:17:32 +0100

    :test_tube: add integration tests [#1]
    Integration tests require connected HW

---
+++ integration_tests/test_app_board_v3_rev0.py
@@ -0,0 +1,25 @@
+import logging
+
+import pytest
+
+from umrx_app_v3.mcu_board.bst_protocol import BstProtocol
+from umrx_app_v3.mcu_board.bst_app_board import BoardInfo
+from umrx_app_v3.mcu_board.app_board_v3_rev0 import ApplicationBoardV3Rev0
+from umrx_app_v3.mcu_board.comm.usb_comm import UsbCommunication
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.mark.app_board
+def test_app_board_v3_rev0_board_info(app_board_v3_rev0: ApplicationBoardV3Rev0):
+    info = app_board_v3_rev0.board_info
+    assert isinstance(info, BoardInfo), "Expecting BoardInfo instance"
+    assert info.board_id == 0x05, "Wrong board ID"
+    assert info.hardware_id == 0x10, "Wrong hardware ID"
+    assert info.software_id == 0x09, "Wrong software ID"
+    # assert info.shuttle_id == 0x66, "Wrong shuttle ID for BMI08x"
+    assert info.shuttle_id == 0x141, "Wrong shuttle ID for BMI08x"
+
+
+# def test_app_board_v3_rev0_switch_app_mtp(app_board_v3_rev0: ApplicationBoardV3Rev0):
+#     ok = app_board_v3_rev0.switch_usb_mtp()
