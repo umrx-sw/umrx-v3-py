b99d59346dd7c2b8bee8ffdcea1e9ff0f2770efa
Konstantin Selyunin <selyunin.k.v@gmail.com>
2024-10-27 12:46:08 +0100

    :sparkles: serial communication [#1]

--- src/umrx_app_v3/mcu_board/comm/serial_comm.py
+++ src/umrx_app_v3/mcu_board/comm/serial_comm.py
@@ -1,6 +1,13 @@
-
+import sys
+import logging
 from umrx_app_v3.mcu_board.comm.comm import Communication
 import serial
+
+logger = logging.getLogger(__name__)
+
+
+class SerialCommunicationError(Exception):
+    ...
 
 
 class SerialCommunication(Communication):
@@ -8,24 +15,67 @@ class SerialCommunication(Communication):
         self.vid = 0x108c  # App Board 3.1
         self.pid = 0xab38
         self.port: serial.Serial | None = None
+        self.port_name: str | None = None
+        self.buffer_size = 125
+        self.is_initialized: bool = False
 
     def send(self, message):
-        pass
+        bytes_written = self.port.write(message)
+        self.port.flush()
+        return bytes_written == len(message)
 
     def receive(self):
-        pass
+        ok = False
+        read_from_serial = bytes()
+        while not ok:
+            # read until we get something in the buffer
+            in_waiting = self.port.in_waiting
+            logger.info(f"waiting buffer: {in_waiting}")
+            read_from_serial += self.port.read(self.buffer_size)
+            logger.info(f"buffer size: {len(read_from_serial)}")
+            ok = len(read_from_serial) > 0
+        return read_from_serial
 
     def send_receive(self, message):
-        pass
+        send_ok = self.send(message)
+        if not send_ok:
+            raise SerialCommunicationError("Sending packet failed!")
+        return self.receive()
 
     def find_device(self):
-        pass
+        match sys.platform:
+            case 'linux':
+                self.find_device_on_linux()
+            case 'win32':
+                raise NotImplementedError(f"Searching device is not implemented for Windows!")
+            case 'darwin':
+                raise NotImplementedError(f"Searching device is not implemented for Mac!")
+            case _:
+                raise NotImplementedError(f"Searching device is not implemented for {sys.platform}!")
+
+    def find_device_on_linux(self):
+        import pyudev
+        context = pyudev.Context()
+        for device in context.list_devices(subsystem='tty'):
+            if device.get('ID_VENDOR') == 'Bosch_Sensortec_GmbH':
+                self.port_name = device.device_node
+                logger.info(f"Found serial device: port={self.port_name}")
+                return True
+        else:
+            return False
 
     def initialize(self):
-        pass
+        if self.port_name is None:
+            self.find_device()
+        self.port = serial.Serial(port=self.port_name)
+        self.port.port = self.port_name
+        self.port.baudrate = 115200
+        if not self.port.is_open:
+            self.port.open()
 
     def connect(self):
-        pass
+        if not self.is_initialized:
+            self.initialize()
 
     def disconnect(self):
         pass
