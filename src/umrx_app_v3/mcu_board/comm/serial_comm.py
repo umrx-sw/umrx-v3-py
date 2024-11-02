e0ee6ec5cce4167997964fa66078da53b72938f3
Konstantin Selyunin <selyunin.k.v@gmail.com>
2024-11-02 11:19:35 +0100

    :wrench: serial communication [#1]
    Update serial communication

--- src/umrx_app_v3/mcu_board/comm/serial_comm.py
+++ src/umrx_app_v3/mcu_board/comm/serial_comm.py
@@ -1,6 +1,7 @@
 import logging
 import sys
 from array import array
+from typing import Any
 
 import serial
 
@@ -13,20 +14,20 @@ class SerialCommunicationError(Exception): ...
 
 
 class SerialCommunication(Communication):
-    def __init__(self) -> None:
-        self.vid = 0x108C  # App Board 3.1
-        self.pid = 0xAB38
+    def __init__(self, **kw: Any) -> None:
+        self.vid = kw["vid"] if kw.get("vid") else 0x108C  # App Board 3.1
+        self.pid = kw["pid"] if kw.get("pid") else 0xAB38
         self.port: serial.Serial | None = None
         self.port_name: str | None = None
         self.buffer_size = 125
         self.is_initialized: bool = False
 
-    def send(self, message: array | list | tuple) -> bool:
+    def send(self, message: array[int] | list[int] | tuple[int, ...]) -> bool:
         bytes_written = self.port.write(message)
         self.port.flush()
         return bytes_written == len(message)
 
-    def receive(self) -> array | bytes:
+    def receive(self) -> array[int] | bytes:
         ok = False
         read_from_serial = b""
         while not ok:
@@ -38,7 +39,7 @@ class SerialCommunication(Communication):
             ok = len(read_from_serial) > 0
         return read_from_serial
 
-    def send_receive(self, message: array | tuple | list) -> array | bytes:
+    def send_receive(self, message: array[int] | tuple[int, ...] | list[int]) -> array | bytes:
         send_ok = self.send(message)
         if not send_ok:
             error_message = "Sending packet failed!"
@@ -64,7 +65,11 @@ class SerialCommunication(Communication):
 
         context = pyudev.Context()
         for device in context.list_devices(subsystem="tty"):
-            if int(device.get("ID_MODEL_ID"), 16) == self.pid and int(device.get("ID_VENDOR_ID"), 16) == self.vid:
+            model_id = device.get("ID_MODEL_ID")
+            vendor_id = device.get("ID_VENDOR_ID")
+            if model_id is None or vendor_id is None:
+                continue
+            if int(model_id, 16) == self.pid and int(vendor_id, 16) == self.vid:
                 self.port_name = device.device_node
                 logger.debug(f"Found board: port={self.port_name}")
                 return True
@@ -72,7 +77,10 @@ class SerialCommunication(Communication):
 
     def initialize(self) -> None:
         if self.port_name is None:
-            self.find_device()
+            found = self.find_device()
+            if not found:
+                error_msg = "No serial port found!"
+                raise SerialCommunicationError(error_msg)
         self.port = serial.Serial(port=self.port_name)
         self.port.port = self.port_name
         self.port.baudrate = 115200
