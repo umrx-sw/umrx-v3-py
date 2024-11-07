d798d4c863c1ca65ca6d2f63ed48610188f403a0
Konstantin Selyunin <selyunin.k.v@gmail.com>
2024-11-07 16:56:38 +0100

    :test_tube: add missing test [#1]

--- tests/commands/test_set_vdd_vddio.py
+++ tests/commands/test_set_vdd_vddio.py
@@ -1,3 +1,5 @@
+from array import array
+
 import pytest
 
 from umrx_app_v3.mcu_board.commands.set_vdd_vddio import SetVddVddioCmd
@@ -10,3 +12,10 @@ def test_app_board_voltage_to_payload(set_vdd_vddio_command: SetVddVddioCmd) -> 
 
     payload = set_vdd_vddio_command.voltage_to_payload(3.3)
     assert payload == (0x0C, 0xE4)
+
+
+@pytest.mark.commands
+def test_app_board_parse(set_vdd_vddio_command: SetVddVddioCmd) -> None:
+    dummy_message = array("B", (0xCA, 0xFE))
+
+    assert set_vdd_vddio_command.parse(dummy_message) is None
