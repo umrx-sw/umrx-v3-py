fbf63f0825309aa29ce0fa45072e7e79c19c9f91
Konstantin Selyunin <selyunin.k.v@gmail.com>
2024-11-08 22:14:12 +0100

    :construction: spi config [#1]

--- src/umrx_app_v3/mcu_board/commands/streaming.py
+++ src/umrx_app_v3/mcu_board/commands/streaming.py
@@ -1,11 +1,27 @@
 import logging
 import struct
 from array import array
+from dataclasses import dataclass, field
 
-from umrx_app_v3.mcu_board.bst_protocol_constants import CommandType, StreamingSamplingUnit
+from umrx_app_v3.mcu_board.bst_protocol_constants import CommandType, MultiIOPin, StreamingSamplingUnit
 from umrx_app_v3.mcu_board.commands.command import Command, CommandError
 
 logger = logging.getLogger(__name__)
+
+
+@dataclass
+class PollingStreamingSpiChannelConfig:
+    id: int | None = None
+    cs_pin: MultiIOPin | None = None
+    sampling_time: int | None = None
+    sampling_unit: StreamingSamplingUnit | None = None
+    register_address: int | None = None
+    bytes_to_read: int | None = None
+
+
+@dataclass
+class PollingStreamingSpiConfig:
+    channel_configs: list[PollingStreamingSpiChannelConfig] = field(default_factory=list)
 
 
 class StopPollingStreamingCmd(Command):
@@ -31,6 +47,8 @@ class StopInterruptStreamingCmd(Command):
 
 
 class ConfigPollingStreamingCmd(Command):
+    polling_streaming_spi_config = PollingStreamingSpiConfig()
+
     @staticmethod
     def assemble(number_of_sensors: int, sampling_time: int, sampling_unit: StreamingSamplingUnit) -> array[int]:
         yield ConfigPollingStreamingCmd.set_sampling_time(
@@ -52,6 +70,54 @@ class ConfigPollingStreamingCmd(Command):
             sampling_unit.value,
         )
         return Command.create_message_from(payload)
+
+    @staticmethod
+    def set_streaming_channel_spi(
+        cs_pin: MultiIOPin,
+        sampling_time: int,
+        sampling_unit: StreamingSamplingUnit,
+        register_address: int,
+        bytes_to_read: int,
+    ) -> array[int]:
+        channel_id = len(ConfigPollingStreamingCmd.polling_streaming_spi_config.channel_configs) + 1
+        config = PollingStreamingSpiChannelConfig(
+            id=channel_id,
+            cs_pin=cs_pin,
+            sampling_time=sampling_time,
+            sampling_unit=sampling_unit,
+            register_address=register_address,
+            bytes_to_read=bytes_to_read,
+        )
+        ConfigPollingStreamingCmd.polling_streaming_spi_config.channel_configs.append(config)
+
+    @staticmethod
+    def assemble_spi_channel_config(channel_config: PollingStreamingSpiChannelConfig) -> array[int]:
+        analog_switch = 1
+        device_address = 0, 0
+        num_blocks = 1
+        payload = (
+            CommandType.DD_CONFIG_STREAM_POLLING.value,
+            channel_config.id,
+            0,
+            channel_config.cs_pin.value,
+            analog_switch,
+            *device_address,
+            *(int(el) for el in struct.pack(">H", channel_config.sampling_time)),
+            channel_config.sampling_unit.value,
+            1,
+            num_blocks,
+            channel_config.register_address,
+            0,
+            channel_config.bytes_to_read,
+            0,
+            0,
+            0,
+            0,
+        )
+        return Command.create_message_from(payload)
+
+    @staticmethod
+    def set_streaming_channel_i2c() -> None: ...
 
     @staticmethod
     def parse(message: array[int]) -> None: ...
--- tests/commands/test_streaming.py
+++ tests/commands/test_streaming.py
@@ -2,22 +2,26 @@ from array import array
 
 import pytest
 
-from umrx_app_v3.mcu_board.bst_protocol_constants import StreamingSamplingUnit, StreamingMode
+from umrx_app_v3.mcu_board.bst_protocol_constants import MultiIOPin, StreamingSamplingUnit
 from umrx_app_v3.mcu_board.commands.command import CommandError
-from umrx_app_v3.mcu_board.commands.streaming import (StopInterruptStreamingCmd, StopPollingStreamingCmd,
-                                                      ConfigPollingStreamingCmd)
+from umrx_app_v3.mcu_board.commands.streaming import (
+    ConfigPollingStreamingCmd,
+    PollingStreamingSpiChannelConfig,
+    StopInterruptStreamingCmd,
+    StopPollingStreamingCmd,
+)
 
 
 @pytest.mark.commands
 def test_command_stop_polling_streaming_assemble(stop_polling_streaming_command: StopPollingStreamingCmd) -> None:
     payload = stop_polling_streaming_command.assemble()
 
-    assert payload == array("B", (0xAA, 0x06, 0x06, 0x00, 0x0D, 0x0A,))
+    assert payload == array("B", (0xAA, 0x06, 0x06, 0x00, 0x0D, 0x0A))
 
 
 @pytest.mark.commands
 def test_command_stop_polling_streaming_parse(stop_polling_streaming_command: StopPollingStreamingCmd) -> None:
-    dummy_response = array('B', (0xCA, 0xFE))
+    dummy_response = array("B", (0xCA, 0xFE))
 
     assert stop_polling_streaming_command.parse(dummy_response) is None
 
@@ -26,24 +30,109 @@ def test_command_stop_polling_streaming_parse(stop_polling_streaming_command: St
 def test_command_stop_interrupt_streaming_assemble(stop_interrupt_streaming_command: StopInterruptStreamingCmd) -> None:
     payload = stop_interrupt_streaming_command.assemble()
 
-    assert payload == array("B", (0xAA, 0x06, 0x0A, 0x00, 0x0D, 0x0A,))
+    assert payload == array("B", (0xAA, 0x06, 0x0A, 0x00, 0x0D, 0x0A))
 
 
 @pytest.mark.commands
 def test_command_stop_interrupt_streaming_parse(stop_interrupt_streaming_command: StopInterruptStreamingCmd) -> None:
-    dummy_response = array('B', (0xCA, 0xFE))
+    dummy_response = array("B", (0xCA, 0xFE))
 
     assert stop_interrupt_streaming_command.parse(dummy_response) is None
 
 
 @pytest.mark.commands
 def test_command_config_polling_streaming(config_polling_streaming_command: ConfigPollingStreamingCmd) -> None:
-    payload = config_polling_streaming_command.assemble(
+    payload = config_polling_streaming_command.set_sampling_time(
         number_of_sensors=2, sampling_time=0x7D, sampling_unit=StreamingSamplingUnit.MICRO_SECOND
     )
-    assert payload == array("B", (0xAA, 0x0A, 0x03, 0x02, 0x01, 0x00, 0x7D, 0x01, 0x0D, 0x0A,))
+    assert payload == array("B", (0xAA, 0x0A, 0x03, 0x02, 0x01, 0x00, 0x7D, 0x01, 0x0D, 0x0A))
 
     with pytest.raises(CommandError):
-        config_polling_streaming_command.assemble(
+        config_polling_streaming_command.set_sampling_time(
             number_of_sensors=5, sampling_time=0x7D, sampling_unit=StreamingSamplingUnit.MICRO_SECOND
         )
+
+
+@pytest.mark.commands
+def test_command_assemble_spi_channel_config(config_polling_streaming_command: ConfigPollingStreamingCmd) -> None:
+    config = PollingStreamingSpiChannelConfig(
+        id=1,
+        cs_pin=MultiIOPin.MINI_SHUTTLE_PIN_2_1,
+        sampling_time=625,
+        sampling_unit=StreamingSamplingUnit.MICRO_SECOND,
+        register_address=0x12,
+        bytes_to_read=7,
+    )
+    payload = config_polling_streaming_command.assemble_spi_channel_config(config)
+
+    expected_payload = array(
+        "B",
+        (
+            0xAA,
+            0x17,
+            0x0F,
+            0x01,
+            0x00,
+            0x16,
+            0x01,
+            0x00,
+            0x00,
+            0x02,
+            0x71,
+            0x01,
+            0x01,
+            0x01,
+            0x12,
+            0x00,
+            0x07,
+            0x00,
+            0x00,
+            0x00,
+            0x00,
+            0x0D,
+            0x0A,
+        ),
+    )
+
+    assert payload == expected_payload
+
+    config = PollingStreamingSpiChannelConfig(
+        id=2,
+        cs_pin=MultiIOPin.MINI_SHUTTLE_PIN_2_5,
+        sampling_time=500,
+        sampling_unit=StreamingSamplingUnit.MICRO_SECOND,
+        register_address=0x02,
+        bytes_to_read=6,
+    )
+    payload = config_polling_streaming_command.assemble_spi_channel_config(config)
+
+    expected_payload = array(
+        "B",
+        (
+            0xAA,
+            0x17,
+            0x0F,
+            0x02,
+            0x00,
+            0x14,
+            0x01,
+            0x00,
+            0x00,
+            0x01,
+            0xF4,
+            0x01,
+            0x01,
+            0x01,
+            0x02,
+            0x00,
+            0x06,
+            0x00,
+            0x00,
+            0x00,
+            0x00,
+            0x0D,
+            0x0A,
+        ),
+    )
+
+    assert payload == expected_payload
