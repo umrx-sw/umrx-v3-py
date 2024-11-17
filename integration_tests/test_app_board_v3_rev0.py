6e760a2bfa4d12b4b5d54449ddf699ec461d87ef
Konstantin Selyunin <selyunin.k.v@gmail.com>
2024-11-17 17:46:14 +0100

    :sparkles: add integration test [#1]
    Integration test for interrupt streaming with MCU time stamp for BMI088 accel and gyro data over SPI.

--- integration_tests/test_app_board_v3_rev0.py
+++ integration_tests/test_app_board_v3_rev0.py
@@ -271,3 +271,76 @@ def test_streaming_interrupt_i2c_accel_and_gyro(app_board_v3_rev0: ApplicationBo
         elif sensor_id == 2:
             logger.info(f"[g] {packet=:06d} {time_stamp=:08d}, g_x={data_x:04d}, g_y={data_y:04d}, g_z={data_z:04d}")
     app_board_v3_rev0.stop_interrupt_streaming()
+
+
+def test_streaming_interrupt_spi_accel_and_gyro(app_board_v3_rev0: ApplicationBoardV3Rev0) -> None:
+    app_board_v3_rev0.initialize()
+    app_board_v3_rev0.start_communication()
+    info = app_board_v3_rev0.board_info
+    assert info.shuttle_id == 0x66, "The integration test works only with BMI088"
+    app_board_v3_rev0.set_pin_config(MultiIOPin.MINI_SHUTTLE_PIN_2_1, PinDirection.OUTPUT, PinValue.HIGH)
+    app_board_v3_rev0.set_pin_config(MultiIOPin.MINI_SHUTTLE_PIN_2_5, PinDirection.OUTPUT, PinValue.HIGH)
+    app_board_v3_rev0.set_pin_config(MultiIOPin.MINI_SHUTTLE_PIN_2_6, PinDirection.OUTPUT, PinValue.LOW)
+    app_board_v3_rev0.set_vdd_vddio(3.3, 3.3)
+    time.sleep(0.2)
+    app_board_v3_rev0.configure_spi()
+    resp = app_board_v3_rev0.read_spi(MultiIOPin.MINI_SHUTTLE_PIN_2_1, 0x0, 1)
+    logger.info("Initial read from accel, otherwise subsequent reads do not work")
+
+    resp = app_board_v3_rev0.read_spi(MultiIOPin.MINI_SHUTTLE_PIN_2_5, 0x0, 1)
+    assert resp[0] == 0x0F, "Expect correct address for BMI088 gyroscope"
+
+    resp = app_board_v3_rev0.read_spi(MultiIOPin.MINI_SHUTTLE_PIN_2_1, 0x0, 2)
+    assert resp[1] == 0x1E, "Expect correct address for BMI088 accelerometer"
+    # power on accelerometer - it is OFF by default
+    app_board_v3_rev0.write_spi(MultiIOPin.MINI_SHUTTLE_PIN_2_1, 0x7C, array("B", (0x00,)))
+    app_board_v3_rev0.write_spi(MultiIOPin.MINI_SHUTTLE_PIN_2_1, 0x7D, array("B", (0x04,)))
+    # configure interrupt for accel
+    int1_io_conf = 0x53
+    app_board_v3_rev0.write_spi(MultiIOPin.MINI_SHUTTLE_PIN_2_1, int1_io_conf, array("B", (0x0A,)))
+    int1_int2_map_data = 0x58
+    app_board_v3_rev0.write_spi(MultiIOPin.MINI_SHUTTLE_PIN_2_1, int1_int2_map_data, array("B", (0x04,)))
+    # set gyro power mode
+    gyro_lpm_addr = 0x11
+    app_board_v3_rev0.write_spi(MultiIOPin.MINI_SHUTTLE_PIN_2_5, gyro_lpm_addr, array("B", (0x00,)))
+    # configure gyro measurement bandwidth
+    gyro_bandwidth_addr = 0x10
+    app_board_v3_rev0.write_spi(MultiIOPin.MINI_SHUTTLE_PIN_2_5, gyro_bandwidth_addr, array("B", (0x01,)))
+    # configure gyro measurement range
+    gyro_range_addr = 0x0F
+    app_board_v3_rev0.write_spi(MultiIOPin.MINI_SHUTTLE_PIN_2_5, gyro_range_addr, array("B", (0x03,)))
+    # map gyro data ready to INT3 pin
+    int3_int4_io_map = 0x18
+    app_board_v3_rev0.write_spi(MultiIOPin.MINI_SHUTTLE_PIN_2_5, int3_int4_io_map, array("B", (0x01,)))
+    # set gyro int3 pin to active high
+    int3_int4_io_conf = 0x16
+    app_board_v3_rev0.write_spi(MultiIOPin.MINI_SHUTTLE_PIN_2_5, int3_int4_io_conf, array("B", (0x51,)))
+    gyro_int_ctrl = 0x15
+    app_board_v3_rev0.write_spi(MultiIOPin.MINI_SHUTTLE_PIN_2_5, gyro_int_ctrl, array("B", (0x80,)))
+    app_board_v3_rev0.enable_timer()
+    time.sleep(0.1)
+    app_board_v3_rev0.streaming_interrupt_set_spi_channel(
+        interrupt_pin=MultiIOPin.MINI_SHUTTLE_PIN_1_6,
+        cs_pin=MultiIOPin.MINI_SHUTTLE_PIN_2_1,
+        register_address=0x12,
+        bytes_to_read=7,
+    )
+    app_board_v3_rev0.streaming_interrupt_set_spi_channel(
+        interrupt_pin=MultiIOPin.MINI_SHUTTLE_PIN_1_7,
+        cs_pin=MultiIOPin.MINI_SHUTTLE_PIN_2_5,
+        register_address=0x02,
+        bytes_to_read=6,
+    )
+    app_board_v3_rev0.configure_streaming_interrupt(interface="spi")
+    app_board_v3_rev0.start_interrupt_streaming()
+    time.sleep(0.5)
+    for _ in range(100):
+        streaming = app_board_v3_rev0.receive_interrupt_streaming(includes_mcu_timestamp=True)
+        sensor_id, packet, time_stamp, payload = streaming
+        if sensor_id == 1:
+            a_x, a_y, a_z = struct.unpack("<xhhh", payload)
+            logger.info(f"[a] {packet=:06d} {time_stamp=:08d}, a_x={a_x:04d}, a_y={a_y:04d}, a_z={a_z:04d}")
+        elif sensor_id == 2:
+            g_x, g_y, g_z = struct.unpack("<hhh", payload)
+            logger.info(f"[g] {packet=:06d} {time_stamp=:08d}, g_x={g_x:04d}, g_y={g_y:04d}, g_z={g_z:04d}")
+    app_board_v3_rev0.stop_interrupt_streaming()
